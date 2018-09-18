744bd67234f60b7688b2c86d4fd85589a95dcfc7
Brynna Conway <brynnaconway@BCONWAY-MBP-SEA.local>
2018-09-18 10:47:39 -0500

    cleaned up code

--- home/home.js
+++ home/home.js
@@ -28,25 +28,25 @@ function initAutocomplete() {
     autocomplete.addListener('place_changed', fillInAddress);
 }
 
-function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
+// function fillInAddress() {
+//     // Get the place details from the autocomplete object.
+//     var place = autocomplete.getPlace();
 
-    for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-    }
+//     for (var component in componentForm) {
+//         document.getElementById(component).value = '';
+//         document.getElementById(component).disabled = false;
+//     }
 
-    // Get each component of the address from the place details
-    // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-            var val = place.address_components[i][componentForm[addressType]];
-            document.getElementById(addressType).value = val;
-        }
-    }
-}
+//     // Get each component of the address from the place details
+//     // and fill the corresponding field on the form.
+//     for (var i = 0; i < place.address_components.length; i++) {
+//         var addressType = place.address_components[i].types[0];
+//         if (componentForm[addressType]) {
+//             var val = place.address_components[i][componentForm[addressType]];
+//             document.getElementById(addressType).value = val;
+//         }
+//     }
+// }
 
 // Bias the autocomplete object to the user's geographical location,
 // as supplied by the browser's 'navigator.geolocation' object.
