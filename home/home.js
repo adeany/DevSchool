7b9af5d717b877302b8681e34eb20299723d76ed
Brynna Conway <brynnaconway@BCONWAY-MBP-SEA.local>
2018-09-18 14:17:08 -0500

    moved add location button over map

--- home/home.js
+++ home/home.js
@@ -16,6 +16,11 @@ var componentForm = {
     postal_code: 'short_name'
 };
 
+function initGoogle() {
+    initMap();
+
+    initAutocomplete();
+}
 function initAutocomplete() {
     // Create the autocomplete object, restricting the search to geographical
     // location types.
@@ -23,30 +28,7 @@ function initAutocomplete() {
             /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
         { types: ['geocode'] });
 
-    // When the user selects an address from the dropdown, populate the address
-    // fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
 }
-
-// function fillInAddress() {
-//     // Get the place details from the autocomplete object.
-//     var place = autocomplete.getPlace();
-
-//     for (var component in componentForm) {
-//         document.getElementById(component).value = '';
-//         document.getElementById(component).disabled = false;
-//     }
-
-//     // Get each component of the address from the place details
-//     // and fill the corresponding field on the form.
-//     for (var i = 0; i < place.address_components.length; i++) {
-//         var addressType = place.address_components[i].types[0];
-//         if (componentForm[addressType]) {
-//             var val = place.address_components[i][componentForm[addressType]];
-//             document.getElementById(addressType).value = val;
-//         }
-//     }
-// }
 
 // Bias the autocomplete object to the user's geographical location,
 // as supplied by the browser's 'navigator.geolocation' object.
@@ -64,4 +46,31 @@ function geolocate() {
             autocomplete.setBounds(circle.getBounds());
         });
     }
+}
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: { lat: -34.397, lng: 150.644 }
+    });
+    var geocoder = new google.maps.Geocoder();
+
+    document.getElementById('submit').addEventListener('click', function () {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
 }
