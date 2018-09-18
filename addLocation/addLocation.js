e1ecd85dc1feff9508e6d9c6fb844c909744d1b6
Brynna Conway <brynnaconway@BCONWAY-MBP-SEA.local>
2018-09-17 21:41:37 -0500

    got rid of some redundancies

--- addLocation/addLocation.js
+++ addLocation/addLocation.js
@@ -3,7 +3,7 @@
 // to the base of the flagpole.
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
       center: {lat: -33.9, lng: 151.2}
     });
@@ -29,7 +29,7 @@ function initMap() {
   
     // Origins, anchor positions and coordinates of the marker increase in the X
     // direction to the right and in the Y direction down.
-    var image = {
+    image = {
       url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
       // This marker is 20 pixels wide by 32 pixels high.
       size: new google.maps.Size(20, 32),
@@ -41,7 +41,7 @@ function initMap() {
     // Shapes define the clickable region of the icon. The type defines an HTML
     // <area> element 'poly' which traces out a polygon as a series of X,Y points.
     // The final coordinate closes the poly by connecting to the first coordinate.
-    var shape = {
+    shape = {
       coords: [1, 1, 1, 20, 18, 20, 18, 1],
       type: 'poly'
     };
@@ -60,24 +60,6 @@ function initMap() {
 
   function addLoc() {
       console.log("Clicked");
-      var image = {
-        url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        // This marker is 20 pixels wide by 32 pixels high.
-        size: new google.maps.Size(20, 32),
-        // The origin for this image is (0, 0).
-        origin: new google.maps.Point(0, 0),
-        // The anchor for this image is the base of the flagpole at (0, 32).
-        anchor: new google.maps.Point(0, 32)
-      };
-      var shape = {
-        coords: [1, 1, 1, 20, 18, 20, 18, 1],
-        type: 'poly'
-      };
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: {lat: -33.9, lng: 151.2}
-      });
-      setMarkers(map);
       var marker = new google.maps.Marker({
         position: {lat: -33.890542,  lng: 151.174856,},
         map: map,
