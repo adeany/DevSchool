620b6ec370af11172b1c5a407d1276e391a2a9db
Alex Deany <alexdeany@ADEANY-MBP-SF.local>
2018-09-20 16:15:37 -0500

    support for multiple markers

--- myapp/src/home/Map.tsx
+++ myapp/src/home/Map.tsx
@@ -1,21 +1,52 @@
 import * as React from 'react';
 import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps';
 
-class Map extends React.Component {
+interface MapProps {
+
+}
+
+interface MapState {
+    markers: Array<any>,
+}
+
+class Map extends React.Component<MapProps, MapState> {
 
     constructor(props: any) {
         super(props);
+
+        this.state = {
+            markers: [
+                { lat: 55.56, lng: -90.78 },
+                { lat: 46.67, lng: -80.87 }
+            ],
+        }
     }
 
-    markerClicked = (event:any) => {
+    markerAdded = (lat:any, lng: any) => {
+        const existingMarkers = this.state.markers;
+        existingMarkers.push({ lat: lat, lng: lng });
+        this.setState({ markers: existingMarkers });
+    }
+
+    markerClicked = (event: any) => {
         console.log('marker was clicked');
+        this.markerAdded(60, -96);
     }
 
     public render() {
 
         const AppMap = withGoogleMap(props =>
             <GoogleMap defaultZoom={8} defaultCenter={{ lat: 40.756795, lng: -73.954298 }} >
-                <Marker position={{ lat: 40.756795, lng: -73.954298 }} onClick={this.markerClicked} />
+                {this.state.markers.map((marker: any) => {
+                    return (
+                        <Marker
+                            position={marker}
+                            title="Click to zoom"
+                            onClick={this.markerClicked}
+                        />
+                    )
+                })}
+                {/* <Marker position={{ lat: 40.756795, lng: -73.954298 }} onClick={this.markerClicked} /> */}
             </GoogleMap>
         );
 
