81a1538bad5c1b07af7bcc2535100d0d10e8afe4
Alex Deany <alexdeany@ADEANY-MBP-SF.local>
2018-09-21 10:07:08 -0500

    added comments

--- myapp/src/home/LocationInfo.tsx
+++ myapp/src/home/LocationInfo.tsx
@@ -3,29 +3,62 @@ import './Home.css';
 import Map from './Map';
 
 interface LocationInfoProps { surveyData: Object };
-class LocationInfo extends React.Component<LocationInfoProps>{
+interface LocationInfoState { commentText: string, addedComments: Array<Object> };
+
+class LocationInfo extends React.Component<LocationInfoProps, LocationInfoState>{
 
     constructor(props: any) {
         super(props);
+
+        this.state = {
+            commentText: '',
+            addedComments: [],
+        }
+
+        this.addComment = this.addComment.bind(this);
+        this.getComments = this.getComments.bind(this);
     }
 
-    public testing = () => {
-        console.log(this.props.surveyData);
+    public getComments = (currentQuestion: string) => {
+        var comments: any = [];
+        this.state.addedComments.forEach((entry: Object) => {
+            if (entry['question'] === currentQuestion) {
+                comments.push(
+                    <li className='list-group-item form-control'>{entry['comment']}</li>
+                );
+            }
+        });
+        return comments;
+    }
+
+    public commentChanged = (event: any) => {
+        this.setState({ commentText: event.target.value });
+    }
+
+    public addComment = (currentQuestion: string) => {
+        var commentText: string = this.state.commentText;
+        if (commentText) {
+            var newComment: Object = { question: currentQuestion, comment: commentText };
+            var comments = this.state.addedComments;
+            comments.push(newComment);
+            this.setState({ addedComments: comments, commentText: '' });
+        }
     }
 
     public renderSurvey() {
         let surveyQuestions: Array<any> = [];
         console.log("data: ", this.props.surveyData);
-        this.props.surveyData['additionalQuestions'].forEach((question:string) => {
+        this.props.surveyData['additionalQuestions'].forEach((question: string) => {
             console.log("question: ", question);
             surveyQuestions.push(<div id="markerInfo" className="container" >
                 <p className="lead" id='locationSurvey'>{question}</p>
-                <ul id="comments" className="list-group commentsGroup" />
+                <br/>
+                <ul id="comments" className="list-group commentsGroup">{this.getComments(question)}</ul>
                 <div className="input-group mb-3">
                     <input type="text" className="form-control" placeholder="Comment goes here..."
-                        aria-describedby="button-addon2" id="newComment" />
+                        aria-describedby="button-addon2" onChange={this.commentChanged} id="newComment" />
                     <div className="input-group-append">
-                        <button className="btn btn-outline-primary" type="button" id="commentBtn" onClick={this.testing}>Comment</button>
+                        <button className="btn btn-outline-primary" type="button" id="commentBtn" onClick={event => this.addComment(question)}>Comment</button>
                     </div>
                 </div>
             </div>)
@@ -40,7 +73,7 @@ class LocationInfo extends React.Component<LocationInfoProps>{
                     id='addButton'> Add Location
         </button>
                 {/* <div id="map" /> */}
-                <Map address={this.props.surveyData['address']}/>
+                <Map address={this.props.surveyData['address']} />
                 <div className='jumbotron'>
                     <h3 className="display-6">Share Your Thoughts!</h3>
                     <p className="lead" id='getStarted'>Click on a red marker to get started.</p>
